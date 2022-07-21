8bb38056dda278e0e62bbcf6d84d0ebaa48c61cd
unknown <dman1999q@gmail.com>
2022-07-21 17:52:33 +0200

    added variable and data type

--- TravelManagement.cpp
+++ TravelManagement.cpp
@@ -3,4 +3,45 @@
 #include <iomanip>
 #include <windows.h>
 
-he
+using namespace std;
+
+class Customers
+{
+    public:
+            string name, gender, address;
+            int age, phoneNo, cusID;
+
+            void detDetails()
+            {
+                cout <<"Enter Customer ID: ";
+                cin >> cusID;
+                cout <<"Enter Name: ";
+                cin >> name;
+                cout <<"Enter Age: ";
+                cin >> age;
+                cout <<"Enter Phone no: ";
+                cin >> phoneNo;
+                cout <<"Enter Address: ";
+                cin >> address;
+                cout <<"Enter Gender: ";
+                cin >> gender;
+            };
+};
+class CABSTR{
+
+};
+
+class Booking
+{
+
+};
+class Charges
+{
+
+};
+
+int main()
+{
+    cout << "Hello World" <<endl;
+    return 0;
+}
