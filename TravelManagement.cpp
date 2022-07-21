25a5e340f45d2c033fa5b88b53514310e3ccd7f7
unknown <dman1999q@gmail.com>
2022-07-21 18:25:12 +0200

    finshed customer class

--- TravelManagement.cpp
+++ TravelManagement.cpp
@@ -10,24 +10,52 @@ class Customers
     public:
             string name, gender, address;
             int age, phoneNo, cusID;
+            char all[999];
 
             void detDetails()
             {
-                cout <<"Enter Customer ID: ";
-                cin >> cusID;
-                cout <<"Enter Name: ";
-                cin >> name;
-                cout <<"Enter Age: ";
-                cin >> age;
-                cout <<"Enter Phone no: ";
-                cin >> phoneNo;
-                cout <<"Enter Address: ";
-                cin >> address;
-                cout <<"Enter Gender: ";
-                cin >> gender;
-            };
+                ofstream out("old-customers.txt", ios::app);
+                {
+                    cout <<"Enter Customer ID: ";
+                    cin >> cusID;
+                    cout <<"Enter Name: ";
+                    cin >> name;
+                    cout <<"Enter Age: ";
+                    cin >> age;
+                    cout <<"Enter Phone no: ";
+                    cin >> phoneNo;
+                    cout <<"Enter Address: ";
+                    cin >> address;
+                    cout <<"Enter Gender: ";
+                    cin >> gender;
+                }
+                out << "\nCustomer ID: " << cusID << "\nName: " << name << "\nAge: " << age << "\nPhone Number: " << phoneNo << "\nAdress :" << address << "\nGender :" << gender << endl;
+                out.close();
+                cout <<"\nSaved \nNote: We save your personal data for no more than 8 months\n" << endl;
+            }
+
+            void showDetails()
+            {
+                ifstream in("old-customers.txt")
+                {
+                    if(!in)
+                    {
+                        cout << "File Error!" <<endl;
+                    }
+                    while (!in.eof())
+                    {
+                        in.getline(all,999);
+                        cout << all << endl;
+                    }
+                    in.close();
+                    
+                }
+            }
+
 };
-class CABSTR{
+
+class Cabs
+{
 
 };
 
